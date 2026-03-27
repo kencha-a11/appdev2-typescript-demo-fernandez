272a2383c356b6656cd1721f4f5a3e54db69ab54
aljon <aljonkenfernandez36@gmail.com>
2026-03-27 14:20:05 +0800

    Done playing and updating the 01-basic.ts file

---
+++ 01-basics.js
@@ -0,0 +1,17 @@
+"use strict";
+// console.log("Hello World!")
+let userName = "Aljon";
+let userAge = 22;
+// ...
+// userAge = "22" // not allowed by typescript
+console.log(`Username - ${userName}`);
+console.log(`Age - ${userAge}`);
+function add(a, b = 5) {
+    return a + b;
+}
+add(10);
+// add('10')
+add(10, 6);
+// add(10, '6')
+console.log(`Added value: ${add(10, 6)}`);
+// console.log(`Added value: ${add(userName, userAge)}`)
--- 01-basics.ts
+++ 01-basics.ts
@@ -1,11 +1,11 @@
 // console.log("Hello World!")
 
-let userName: string = "Elmer";
-let userAge = 31;
+let userName: string = "Aljon";
+let userAge = 22;
 
 // ...
 
-userAge = "31"
+// userAge = "22" // not allowed by typescript
 
 console.log(`Username - ${userName}`)
 console.log(`Age - ${userAge}`)
@@ -18,3 +18,6 @@ add(10)
 // add('10')
 add(10, 6)
 // add(10, '6')
+
+console.log(`Added value: ${add(10, 6)}`)
+// console.log(`Added value: ${add(userName, userAge)}`)
